f9fdd26a841245fc82aaa810f4be17b332c96a5f
MuhammadUsman0007 <141634045+MuhammadUsman0007@users.noreply.github.com>
2023-08-07 16:17:55 +0500

    Add files via upload
    it is an brand name generator which usually ask user city and pet name and generate brand name

---
+++ brand generator.py
@@ -0,0 +1,5 @@
+print("welcome to Band Name Generator.")
+name = input("what is name of city you grow up in ?\n")
+pet = input("what's your pet name\n")
+print("your brand name could be " + name + " " + pet 
+)
